38e9fe649b7e2520edf8bea44820eb77e0fc6b0a
wangcaoyu <wangcaoyu@huawei.com>
2023-12-06 11:35:19 +0800

    添加diff工具自动化测试用例调试
    Signed-off-by: wangcaoyu <wangcaoyu@huawei.com>

--- build-tools/capi_parser/test/testCase/run_main.py
+++ build-tools/capi_parser/test/testCase/run_main.py
@@ -20,6 +20,7 @@ import json
 import unittest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src")))
 from coreImpl.check import check
+from coreImpl.diff import diff_file
 
 
 class TestMethods(unittest.TestCase):
@@ -48,6 +49,33 @@ class TestMethods(unittest.TestCase):
                                  f"\n{test_case_file_name} case is error.\n" +
                                  f"{output_file_name} is out file.\n" +
                                  f"{expect_file_name} is expect out file")
+
+    def test_diff(self):
+        test_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..\\"))
+        test_case_old_path = os.path.join(test_path, "ut\\diff\\old")
+        test_case_new_path = os.path.join(test_path, "ut\\diff\\new")
+        output_path = os.path.join(test_path, "output\\diff")
+        expect_path = os.path.join(test_path, "expect\\diff")
+        old_file_list = os.listdir(test_case_old_path)
+        for case_name in old_file_list:
+            output_file_name = os.path.join(output_path, "{}.json".format(case_name))
+            test_case_old_dir = os.path.join(test_case_old_path, case_name)
+            test_case_new_dir = os.path.join(test_case_new_path, case_name)
+            diff_result = diff_file.global_assignment(test_case_old_dir, test_case_new_dir)
+            diff_json = diff_file.result_to_json(diff_result)
+            diff_file.write_in_txt(diff_json, output_file_name)
+            expect_file_name = os.path.join(expect_path, "{}.json".format(case_name))
+            if not os.path.exists(expect_file_name):
+                continue
+            expect_file = open(expect_file_name)
+            expect_file_content = expect_file.read()
+            expect_file.close()
+            self.assertEqual(diff_json, expect_file_content,
+                             f"\n{case_name} case is error.\n" +
+                             f"\n{test_case_old_dir} is old case dir.\n" +
+                             f"\n{test_case_new_dir} is new case dir.\n" +
+                             f"{output_file_name} is out file.\n" +
+                             f"{expect_file_name} is expect out file")
 
 
 if __name__ == '__main__':
