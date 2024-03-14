12d1e9d035d72b10f88267725d55b53316b8ec34
zhubingwei <zhubingwei@huawei.com>
2024-03-14 15:38:42 +0800

    【图片编解码】修改代码
    Signed-off-by: zhubingwei <zhubingwei@huawei.com>

--- multimedia/image_framework/include/image_format_convert_mdk.h
+++ multimedia/image_framework/include/image_format_convert_mdk.h
@@ -35,33 +35,22 @@
 
 #include "napi/native_api.h"
 #include "image_mdk_common.h"
+#include "pixelmap2_impl.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
- * @brief Defines a native image format convert object for the image format convert APIs.
+ * @brief Convert image format from RGB to YUV by PixelMap.
  *
- * @Syscap SystemCapability.Multimedia.Image
- * @since 12
- */
-struct ImageFormatConvertNative_;
-/**
- * @brief Defines a native image format convert object for the image format convert APIs.
- *
- * @Syscap SystemCapability.Multimedia.Image
- * @since 12
- */
-typedef struct ImageFormatConvertNative_ ImageFormatCovnertNative;
-ImageFormatCovnertNative* OH_ImageCovnertNative_InitNative();
-int32_t OH_ImageCovnertNative_ReleaseNative(ImageFormatCovnertNative* native);
-/**
- * @brief Creates an <b>ImageFormatConvert</b> object at the JavaScript native layer.
- *
+ * @param srcPixelMap Source Image PixelMap
+ * @param destPixelMap Destination Image PixelMap
  * @return Returns {@link IRNdkErrCode} IMAGE_RESULT_SUCCESS - if the operation is successful.
+ *
+ * @Syscap SystemCapability.Multimedia.Image
  */
-int32_t OH_ImageConvert_Create();
+int32_t OH_ImageConvert_YuvToRgb(OH_PixelMap2 *srcPixelMap, OH_PixelMap2 **destPixelMap, int32_t destPixelFormat);
 
 /**
  * @brief Convert image format from RGB to YUV by PixelMap.
@@ -72,57 +61,7 @@ int32_t OH_ImageConvert_Create();
  *
  * @Syscap SystemCapability.Multimedia.Image
  */
-int32_t OH_ImageConvert_YuvToRgb(ImageFormatCovnertNative *srcPixelMap, ImageFormatCovnertNative *destPixelMap,
-                                 int32_t destPixelFormat);
-
-/**
- * @brief Convert image format from RGB to YUV by PixelMap.
- *
- * @param srcPixelMap Source Image PixelMap
- * @param destPixelMap Destination Image PixelMap
- * @return Returns {@link IRNdkErrCode} IMAGE_RESULT_SUCCESS - if the operation is successful.
- *
- * @Syscap SystemCapability.Multimedia.Image
- */
-int32_t OH_ImageConvert_RgbToYuv(ImageFormatCovnertNative *srcPixelMap, ImageFormatCovnertNative *destPixelMap,
-                                 int32_t destPixelFormat);
-
-/**
- * @brief Releases an <b>ImageFormatConvertNative</b> object.
- *
- * @return Returns {@link IRNdkErrCode} IMAGE_RESULT_SUCCESS - if the operation is successful.
- *
- * @Syscap SystemCapability.Multimedia.Image
- * @since 10
- * @version 4.0
- */
-int32_t OH_ImageConvert_Release();
-
-/**
- * @brief Convert Js objects to C pointers.
- **@param env Indicates the pointer to the JavaScript Native Interface (JNI) environment
- * @param pixelMapValue Indicates a JavaScript native API <b>PixelMapNapi</b> object
- * @param pixelMap Indicates the result after conversion
- * @return Returns {@link IRNdkErrCode} IMAGE_RESULT_SUCCESS - if the operation is successful.
- *
- * @Syscap SystemCapability.Multimedia.Image
- * @since 12
- * @version 4.0
- */
-int32_t OH_ImageConvert_JsToC_PixelMap(napi_env env, napi_value pixelMapValue, ImageFormatCovnertNative *pixelMap);
-
-/**
- * @brief Convert Js objects to C pointers.
- **@param env Indicates the pointer to the JavaScript Native Interface (JNI) environment
- * @param pixelMap Indicates a C native API <b>PixelMap</b> object pointer
- * @param result Indicates the result after conversion
- * @return Returns {@link IRNdkErrCode} IMAGE_RESULT_SUCCESS - if the operation is successful.
- *
- * @Syscap SystemCapability.Multimedia.Image
- * @since 12
- * @version 4.0
- */
-int32_t OH_ImageConvert_CToJs_PixelMap(napi_env env, ImageFormatCovnertNative *pixelMap, napi_value *result);
+int32_t OH_ImageConvert_RgbToYuv(OH_PixelMap2 *srcPixelMap, OH_PixelMap2 **destPixelMap, int32_t destPixelFormat);
 
 #ifdef __cplusplus
 };
