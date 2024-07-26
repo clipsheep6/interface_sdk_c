322ecbf64c79a3ceb8aa918335efdbb3cb650e33
zhouhaifeng <kutcher.zhou@huawei.com>
2024-07-26 19:45:57 +0800

    add the api of graphics memory
    Signed-off-by: zhouhaifeng <kutcher.zhou@huawei.com>

--- hiviewdfx/hidebug/include/hidebug/hidebug.h
+++ hiviewdfx/hidebug/include/hidebug/hidebug.h
@@ -139,24 +139,13 @@ HiDebug_ErrorCode OH_HiDebug_StartAppTraceCapture(HiDebug_TraceFlag flag,
 HiDebug_ErrorCode OH_HiDebug_StopAppTraceCapture();
 
 /**
- * @brief Get the memory of application by item.
+ * @brief Get the graphics memory of application.
  *
- * @param item Indicates memory item to {@link HiDebug_MemoryItem}
- * @param value Indicates value of memory item
+ * @param value Indicates value of graphics memory
  * @return Returns {@code HIDEBUG_SUCCESS} if successful. See {@link HiDebug_ErrorCode}
  * @since 13
  */
-HiDebug_ErrorCode OH_HiDebug_GetApMemoryStat(HiDebug_MemoryItem item, uint32_t *value);
-
-/**
- * @brief Get the memory of application.
- *
- * @param appMemoryItem Indicates memory item to {@link HiDebug_AppMemoryItemPtr}
- * @param size Indicates size of HiDebug_AppMemoryItem
- * @return Returns {@code HIDEBUG_SUCCESS} if successful. See {@link HiDebug_ErrorCode}
- * @since 13
- */
-HiDebug_ErrorCode OH_HiDebug_GetApMemoryStats(HiDebug_AppMemoryItemPtr *appMemoryItem, uint32_t *size);
+HiDebug_ErrorCode OH_HiDebug_GetGraphicsMemory(uint32_t *value);
 
 #ifdef __cplusplus
 }
