15950944127cdb77557264deed356b3416039f06
zhaoyuanchun <zhaoyuanchun@huawei.com>
2024-08-12 11:53:07 +0800

    旋转ndk接口d.ts
    Signed-off-by: zhaoyuanchun <zhaoyuanchun@huawei.com>

--- multimedia/camera_framework/video_output.h
+++ multimedia/camera_framework/video_output.h
@@ -187,6 +187,20 @@ Camera_ErrorCode OH_VideoOutput_GetActiveProfile(Camera_VideoOutput* videoOutput
  */
 Camera_ErrorCode OH_VideoOutput_DeleteProfile(Camera_VideoProfile* profile);
 
+/**
+ * @brief Get the videeo rotation angle.
+ *
+ * @param videoOutput the {@link Camera_VideoOutput} instance which used to get the video rotation angle.
+ * @param deviceDegree the current device rotation degree.
+ * @param imageRotation the {@link Camera_ImageRotation} result of video rotation angle.
+ * @return {@link #CAMERA_OK} if the method call succeeds.
+ *         {@link #CAMERA_INVALID_ARGUMENT} if parameter missing or parameter type incorrect.
+ *         {@link #CAMERA_SERVICE_FATAL_ERROR} if camera service fatal error.
+ * @since 12
+ */
+Camera_ErrorCode OH_PhotoOutput_GetVideoRotation(Camera_VideoOutput* videoOutput, int deviceDegree,
+    Camera_ImageRotation* imageRotation);
+
 #ifdef __cplusplus
 }
 #endif
