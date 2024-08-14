ce46fd94d708c3e3b7eb16c0236b5a4e521c941a
liushang <liushang2@huawei.com>
2024-08-14 10:40:44 +0800

    add none = -1
    Signed-off-by: liushang <liushang2@huawei.com>

--- graphic/graphic_2d/native_buffer/buffer_common.h
+++ graphic/graphic_2d/native_buffer/buffer_common.h
@@ -138,7 +138,7 @@ typedef enum OH_NativeBuffer_MetadataType {
     /** HDR VIVID */
     OH_VIDEO_HDR_VIVID,
     /** NONE */
-    OH_VIDEO_NONE,
+    OH_VIDEO_NONE = -1
 } OH_NativeBuffer_MetadataType;
 
 /**
