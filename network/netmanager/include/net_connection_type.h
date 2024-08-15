778e1790714fe6d7d73cda5adaea826bcb4144ae
NoCodeNoBB <maoyufeng3@huawei.com>
2024-08-15 09:39:15 +0000

    网络管理新增枚举
    Signed-off-by: NoCodeNoBB <maoyufeng3@huawei.com>

--- network/netmanager/include/net_connection_type.h
+++ network/netmanager/include/net_connection_type.h
@@ -73,7 +73,12 @@ typedef enum NetConn_NetCap {
     * Portal
     * @since 12
     */
-    NETCONN_NET_CAPABILITY_PORTAL = 17
+    NETCONN_NET_CAPABILITY_PORTAL = 17,
+    /**
+     * In checking network connectivity.
+     * @since 12
+     */
+    NETCONN_NET_CAPABILITY_CHECKING_CONNECTIVITY = 31
 } NetConn_NetCap;
 
 /**
@@ -85,6 +90,11 @@ typedef enum NetConn_NetCap {
 typedef enum NetConn_NetBearerType {
     /** Cellular network */
     NETCONN_BEARER_CELLULAR = 0,
+    /**
+     * Bluetooth
+     * @since 12
+     */
+    NETCONN_BEARER_BLUETOOTH = 2,
     /** WIFI */
     NETCONN_BEARER_WIFI = 1,
     /** Ethernet */
